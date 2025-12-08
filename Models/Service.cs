be8c1addc3f98afce50cc74d60b8a4c3f07cf894
Tim Middleton <middletontimothy53@gmail.com>
2025-12-08 14:23:27 -0600

    Added Service Model Class

---
+++ Models/Service.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS212FinalProject.Models
+{
+    public class Service
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; }
+    }
+}
