d4f45a2a29eb5b0a8fc315ea1c302cdffe10b35a
Jesse Dahlke <jesse.dahlke@ndsu.edu>
2025-12-08 14:15:00 -0600

    Added SCRUM-33; User class

---
+++ models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS212FinalProject.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(25)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10)]
+        public decimal PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Role { get; set; }
+    }
+}
